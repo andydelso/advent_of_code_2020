9640202a19664fd622de8e71167101105699bebf
Delso, Andy (CHICO) <andrew.delso@hyatt.com>
2020-12-16 13:55:26 -0600

    add day 3 scaffolding

---
+++ src/main/kotlin/day3/Day3Challenge.kt
@@ -0,0 +1,22 @@
+package day3
+
+import DailyChallenge
+import java.io.File
+
+/**
+ * From: https://adventofcode.com/2020/day/2
+ */
+class Day3Challenge(private val file: File) : DailyChallenge {
+
+    /**
+     * Part 1
+     */
+    override fun solvePart1() {
+    }
+
+    /**
+     * Part 2
+     */
+    override fun solvePart2() {
+    }
+}
--- src/test/kotlin/DailyChallengeTests.kt
+++ src/test/kotlin/DailyChallengeTests.kt
@@ -1,6 +1,6 @@
 import day1.Day1Challenge
 import day2.Day2Challenge
-import day2.Day3Challenge
+import day3.Day3Challenge
 import org.junit.Test
 import java.nio.file.Paths
 
@@ -16,6 +16,12 @@ class DailyChallengeTests {
     @Test fun checkSolution2() {
         val day = Day2Challenge(Paths.get("src/main/kotlin/day2/input.txt").toFile())
         print("\n\n*** DAY 2 ***\n")
+        print("Part 1 = ${day.solvePart1()}\n")
+        print("Part 2 = ${day.solvePart2()}\n\n")
+    }
+    @Test fun checkSolution3() {
+        val day = Day3Challenge(Paths.get("src/main/kotlin/day3/input.txt").toFile())
+        print("\n\n*** DAY 3 ***\n")
         print("Part 1 = ${day.solvePart1()}\n")
         print("Part 2 = ${day.solvePart2()}\n\n")
     }
