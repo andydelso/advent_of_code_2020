23792f858f89f68748874eab7fc3a6176743f570
Delso, Andy (CHICO) <andrew.delso@hyatt.com>
2020-12-16 13:52:05 -0600

    make just one test file with all days

---
+++ src/test/kotlin/DailyChallengeTests.kt
@@ -0,0 +1,22 @@
+import day1.Day1Challenge
+import day2.Day2Challenge
+import day2.Day3Challenge
+import org.junit.Test
+import java.nio.file.Paths
+
+class DailyChallengeTests {
+
+    @Test fun checkSolution1() {
+        val day = Day1Challenge(Paths.get("src/main/kotlin/day1/input.txt").toFile())
+        print("\n\n*** DAY 1 ***\n")
+        print("Part 1 = ${day.solvePart1()}\n")
+        print("Part 2 = ${day.solvePart2()}\n\n")
+    }
+
+    @Test fun checkSolution2() {
+        val day = Day2Challenge(Paths.get("src/main/kotlin/day2/input.txt").toFile())
+        print("\n\n*** DAY 2 ***\n")
+        print("Part 1 = ${day.solvePart1()}\n")
+        print("Part 2 = ${day.solvePart2()}\n\n")
+    }
+}
